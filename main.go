24128f0d46eacdaf8113472afcda6b7fa5b48e07
miodawn <778942292@qq.com>
2020-07-20 16:39:53 +0800

    改进：使用结构体变量Conf保存配置信息

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 	"web_app/routes"
 	"web_app/settings"
 
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
 
@@ -28,7 +27,7 @@ func main() {
 	}
 
 	// 2.初始化日志
-	if err := logger.Init(); err != nil {
+	if err := logger.Init(settings.Conf.LogConfig); err != nil {
 		fmt.Printf("init logger failed, err:%v\n", err)
 		return
 	}
@@ -36,14 +35,14 @@ func main() {
 	zap.L().Debug("logger init success...")
 
 	// 3.初始化MySQL
-	if err := mysql.Init(); err != nil {
+	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
 		fmt.Printf("init mysql failed, err:%v\n", err)
 		return
 	}
 	defer mysql.Close()
 
 	// 4.初始化Redis连接
-	if err := redis.Init(); err != nil {
+	if err := redis.Init(settings.Conf.RedisConfig); err != nil {
 		fmt.Printf("init redis failed, err:%v\n", err)
 		return
 	}
@@ -54,7 +53,7 @@ func main() {
 
 	// 6.启动服务(关机保护)
 	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", viper.GetInt("app.port")),
+		Addr:    fmt.Sprintf(":%d", settings.Conf.Port),
 		Handler: r,
 	}
 
--- settings/settings.go
+++ settings/settings.go
@@ -7,10 +7,50 @@ import (
 	"github.com/spf13/viper"
 )
 
+//Conf 全局变量指针，用来保存程序的所有配置信息
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mode         string `mapstructure:"mode"`
+	Version      string `mapstructure:"version"`
+	Port         int    `mapstructure:"port"`
+	*LogConfig   `mapstructure:"log"`
+	*MySQLConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DbName       string `mapstructure:"dbname"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db"`
+	Port     int    `mapstructure:"port"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
 func Init() (err error) {
-	viper.AddConfigPath(".")      // 指定查找配置文件的路径
-	viper.SetConfigName("config") // 指定配置文件
-	viper.SetConfigType("yaml")   // 指定配置文件
+	viper.SetConfigFile("./config/config.json") //main函数目录的相对文件位置，注意使用此函数，下面两行会覆盖你的设置，建议二选一
+	//viper.AddConfigPath(".")                    // 指定查找配置文件的路径
+	//viper.SetConfigName("config")               // 指定配置文件(不需要带后缀)
+
+	//因为字节流中没有文件扩展名，所以支持的扩展名是“ json”，“ toml”，“ yaml”，“ yml”，“ properties”，“ props”，“ prop”，“ env”，“ dotenv”
+	//viper.SetConfigType("yaml") // 指定配置文件(专用于从远程获取配置信息时指定配置文件类型的)
 
 	err = viper.ReadInConfig() // 读取配置信息
 	if err != nil {            // 读取配置信息失败
@@ -18,10 +58,17 @@ func Init() (err error) {
 		fmt.Printf("viper.ReadInConfig() failed, err:%v\n", err)
 		return
 	}
+	// 把读取到的配置信息反序列化到 Conf 变量中
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal() failed, err:%v\n", err)
+	}
 	// 监控配置文件变化
 	viper.WatchConfig()
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		fmt.Println("配置文件修改了...")
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("viper.Unmarshal() failed, err:%v\n", err)
+		}
 	})
 	return
 }
